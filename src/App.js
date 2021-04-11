5ba502042242b0561d44aede464858f1e2dbf035
odedron100 <odedron84@gmail.com>
2021-04-11 12:49:58 +0300

    sad

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Switch,
   Route,
   Redirect
 } from "react-router-dom";
 
 import './assets/styles/main.scss'
-import {HomePage} from './views/HomePage';
+import { HomePage } from './views/HomePage';
 // import { ContactDetailsPage } from './views/ContactDetailsPage';
 import { StatisticPage } from './views/StatisticPage';
 import { ContactPage } from './views/ContactPage';
@@ -18,29 +18,29 @@ import { AppHeader } from './cmps/AppHeader';
 import './App.css';
 // import './AppHeader.scss';
 
-function App () {
-  const PrivateRoute = (props) =>{
+function App() {
+  const PrivateRoute = (props) => {
     const isLoggedin = sessionStorage.getItem('loggedinUser') || false;
-    return isLoggedin ? <Route component={props.component} path={props.path}/> : <Redirect to ="/signup"/>
+    return isLoggedin ? <Route component={props.component} path={props.path} /> : <Redirect to="/signup" />
   }
-    return (
-      <div className="app">
-        <Router>
-          <AppHeader />
-          <div className="main-container">
+  return (
+    <div className="app">
+      <Router>
+        <AppHeader />
+        <div className="main-container">
           <Switch>
-            <Route path='/statistic' component={StatisticPage}/>
+            <Route path='/statistic' component={StatisticPage} />
             <PrivateRoute path='/contacts/edit/:id?' component={ContactEdit} />
             <PrivateRoute path='/contacts/:id' component={ContactDetailsPage} />
             {/* </Route> */}
             <Route path='/signup' component={SignupPage} />
-            <PrivateRoute path='/contacts' component={ContactPage}/>
-            <PrivateRoute path='/' component={HomePage}/>
+            <PrivateRoute path='/contacts' component={ContactPage} />
+            <PrivateRoute path='/' component={HomePage} />
           </Switch>
-          </div>
-        </Router>
-      </div>
-    );
-  }
+        </div>
+      </Router>
+    </div>
+  );
+}
 
 export default App;
